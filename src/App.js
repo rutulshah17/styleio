b5d0b0e79c861050fceecb6d6990c208c34b94f4
Rutul Shah <rutulshah17@gmail.com>
2019-12-17 10:16:18 -0500

    modified app.js so that it has lifecycle methods for signin and signout feature

--- src/App.js
+++ src/App.js
@@ -7,17 +7,47 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop-page.component';
 
-function App() {
-	return (
-		<div>
-			<Header />
-			<Switch>
-				<Route exact path="/" component={HomePage} />
-				<Route path="/shop" component={ShopPage} />
-				<Route path="/signin" component={SignInAndSignUpPage} />
-			</Switch>
-		</div>
-	);
+//de-structing auth, since we do not the whole object
+import { auth } from './firebase/firebase.utils';
+
+class App extends React.Component {
+	
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			currentUser: null
+		}
+	}
+
+	//setting the property to avoid memory leaks, so that we can call this property once user has signed out
+	unsubscribeFromAuth = null
+
+	componentDidMount() {
+		//assigning the function to the property so that we can empty it again in componentWillUnmount()
+		//onAuthStateChanged will continue to send the object, we would have to stop it from giving us
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+			this.setState({ currentUser: user });
+		});
+	}
+
+	componentWillUnmount() {
+		//user have clicked signout, so do not need to listen to onAuthStateChanged anymore
+		this.unsubscribeFromAuth();
+	}
+	
+	render() {
+		return (
+			<div>
+				<Header />
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route path="/shop" component={ShopPage} />
+					<Route path="/signin" component={SignInAndSignUpPage} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
