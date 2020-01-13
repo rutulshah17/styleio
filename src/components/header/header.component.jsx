2c2d04f1e2408d0034864387f96149c7f95d627d
Rutul Shah <rutulshah17@gmail.com>
2020-01-13 09:16:05 -0500

    added cart-dropdown component, which opens up a panel when shopping cart button is clicked, no click functionality yet !!

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,6 +8,7 @@ import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import './header.styles.scss';
 
@@ -30,8 +31,10 @@ const Header = ( {currentUser} ) => (
 			}
 
 			<CartIcon />
-
 		</div>
+
+		<CartDropdown />
+
 	</div>
 ); 
 
