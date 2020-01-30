a100043cf8055d019f6cdeec722463f7421e5209
Rutul Shah <rutulshah17@gmail.com>
2020-01-30 13:16:22 -0500

    added reduce function in cart-icon component to show the sum / number of items added into the cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,9 +7,7 @@ import CustomButton from '../custom-button/custom-button.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => {
-
-    return(
+const CartDropdown = ({ cartItems }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             { cartItems.map( cartItem => 
@@ -18,8 +16,7 @@ const CartDropdown = ({ cartItems }) => {
         </div>
         <CustomButton> Go TO CHECKOUT </CustomButton>
     </div>
-
-)};
+);
 
 const mapStateToProps = state => ({
     cartItems: state.cart.cartItems
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -2,5 +2,4 @@ const CartActionTypes = {
     TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
     ADD_ITEM: 'ADD_ITEM'
 };
-
 export default CartActionTypes;
