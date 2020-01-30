5dbcc5a684f40380556d7e97f78e89b629cbb8e4
Rutul Shah <rutulshah17@gmail.com>
2020-01-30 13:58:01 -0500

    adding couple of console statements to mapState in cart-icon component, have to look into caching / memoization

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -8,22 +8,29 @@ import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden, itemCount }) => (
-	<div className='cart-icon' onClick={toggleCartHidden}>
-		<ShoppingIcon className='shopping-icon' />
-		<span className='item-count'>{ itemCount }</span>
-	</div>
-);
+const CartIcon = ({ toggleCartHidden, itemCount, mapStateCart }) => 
+	{
+		const { cartItems, hidden } = mapStateCart;
+		console.log(cartItems, hidden);
+	
+		return (
+			<div className='cart-icon' onClick={toggleCartHidden}>
+				<ShoppingIcon className='shopping-icon' />
+				<span className='item-count'>{ itemCount }</span>
+			</div>
+)};
 
 const mapDispatchToProps = dispatch => ({
 	toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
 //state.cart.cartItems
+//state.user.currentUser
 const mapStatetoProps = (state) => ({
 	itemCount: state.cart.cartItems.reduce(
 		(accumulatedQuantity, cartItem ) => 
-		accumulatedQuantity + cartItem.quantity, 0)
+		accumulatedQuantity + cartItem.quantity, 0),
+	mapStateCart: state.cart
 });
 
 export default connect(mapStatetoProps, mapDispatchToProps)(CartIcon);
