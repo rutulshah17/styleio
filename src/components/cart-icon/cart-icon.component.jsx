2d88ec757734efb4d355d1de54e2dc7a31cc42d6
Rutul Shah <rutulshah17@gmail.com>
2020-01-30 13:32:46 -0500

    added reduce function in cart-icon component to show the sum / number of items added into the cart, successfully

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -9,21 +9,21 @@ import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
-    <div className='cart-icon' onClick={toggleCartHidden}>
-        <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>{ itemCount }</span>
-    </div>
+	<div className='cart-icon' onClick={toggleCartHidden}>
+		<ShoppingIcon className='shopping-icon' />
+		<span className='item-count'>{ itemCount }</span>
+	</div>
 );
 
 const mapDispatchToProps = dispatch => ({
-    toggleCartHidden: () => dispatch(toggleCartHidden())
+	toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
 //state.cart.cartItems
 const mapStatetoProps = (state) => ({
-    itemCount: state.cart.cartItems.reduce(
-        (accumulatedQuantity, cartItem ) => 
-        accumulatedQuantity + cartItem.quantity, 0)
+	itemCount: state.cart.cartItems.reduce(
+		(accumulatedQuantity, cartItem ) => 
+		accumulatedQuantity + cartItem.quantity, 0)
 });
 
 export default connect(mapStatetoProps, mapDispatchToProps)(CartIcon);
