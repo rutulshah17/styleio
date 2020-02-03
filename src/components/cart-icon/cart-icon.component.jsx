200d0c53cf9907d94751d8ab0c6760d07c6b4315
Rutul Shah <rutulshah17@gmail.com>
2020-02-03 16:27:05 -0500

    with memoization, cart-dropdown components does not re-render on changing state for some other component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,18 +3,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
-
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors'; 
 
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden, itemCount, mapStateCart }) => 
+const CartIcon = ({ toggleCartHidden, itemCount}) => 
 	{
-		const { cartItems, hidden } = mapStateCart;
-		console.log(cartItems, hidden);
-	
 		return (
 			<div className='cart-icon' onClick={toggleCartHidden}>
 				<ShoppingIcon className='shopping-icon' />
@@ -29,8 +25,7 @@ const mapDispatchToProps = dispatch => ({
 //state.cart.cartItems
 //state.user.currentUser
 const mapStatetoProps = (state) => ({
-	itemCount: selectCartItemsCount(state),
-	mapStateCart: state.cart
+	itemCount: selectCartItemsCount(state)
 });
 
 export default connect(mapStatetoProps, mapDispatchToProps)(CartIcon);
