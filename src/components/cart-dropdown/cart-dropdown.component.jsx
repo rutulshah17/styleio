4bf5311cdf9e1879be1e5334fb4cdd4cb130feca
Rutul Shah <rutulshah17@gmail.com>
2020-02-04 13:08:39 -0500

    loading new text in cart-dropdown when empty and added some styles to it

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -12,9 +12,14 @@ import './cart-dropdown.styles.scss';
 const CartDropdown = ({ cartItems }) => (
 	<div className='cart-dropdown'>
 		<div className='cart-items'>
-			{ cartItems.map( cartItem => 
-			  ( <CartItem key={cartItem.id} item={cartItem} /> ) 
-			)} 
+			{ 
+				cartItems.length ?
+				cartItems.map( cartItem => 
+			  		( <CartItem key={cartItem.id} item={cartItem} /> ) 
+				)
+				: ( <span className='empty-message'> Your cart is empty </span> )
+				
+			} 
 		</div>
 		<CustomButton> Go TO CHECKOUT </CustomButton>
 	</div>
