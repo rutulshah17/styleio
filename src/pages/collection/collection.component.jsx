c540b1c99328eb292d2c8fae7c7b369db24fa1c3
Rutul Shah <rutulshah17@gmail.com>
2020-02-25 13:14:52 -0500

    completed collection page component, items now renders based on url

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,26 +2,31 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 
-//import CollectionItem from '../../components/collection-item';
+import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import { selectCollection } from '../../redux/shop/shop.selectors'
 
 import './collection.styles.scss';
 
-const CollectionPage = ( {selectCollection, match} ) => {
+const CollectionPage = ( {mappingCollectionNamewithID} ) => {
 
-   console.log(selectCollection);
+    const { title, items } = mappingCollectionNamewithID;
 
     return (
         <div className='collection-page'>
-            <h2> collection page </h2>
+            <h2 className='title'> {title} </h2>
+            <div className='items'>
+                { items.map ( item => 
+                    <CollectionItem key={item.id} item={item} />
+                ) } 
+            </div>
         </div>
     );
 }
 
 //mapStateToProps also have access to its own props
 const mapStateToProps = (state, ownProps) => ({
-    selectCollection: selectCollection(ownProps.match.params.collectionId)(state)
+    mappingCollectionNamewithID: selectCollection(ownProps.match.params.collectionId)(state)
 });
 
 export default connect(mapStateToProps)(CollectionPage);
