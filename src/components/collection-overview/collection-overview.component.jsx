9cc78c119d579f56a7fb2bb3f940db9a31a3ab39
Rutul Shah <rutulshah17@gmail.com>
2020-02-25 14:16:15 -0500

    spelling mistake in shop.data.js, jacket -> jackets

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -8,15 +8,18 @@ import './collection-overview.styles.scss'
 
 import CollectionPreview from '../collection-preview/collection-preview.component'
 
-const CollectionOverview = ( {collections} ) => (
-    <div className='collection-overview'>  
-    {   
-        collections.map( ( {id, ...otherCollectionProps} ) => 
-            <CollectionPreview key={id} {...otherCollectionProps} />
-        )
-    }
-    </div>
-);
+const CollectionOverview = ( {collections} ) => {
+    console.log(collections);
+    return (
+        <div className='collection-overview'>  
+        {   
+            collections.map( ({ id, ...otherCollectionProps }) => (
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))
+        }
+        </div>
+    );
+}
 
 const mapStateToProps = state => ({
 	collections: selectShopCollection(state)
